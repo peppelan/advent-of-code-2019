14f194475d50e3ff12bce53d55175ebd5fe9af67
Giuseppe Landolfi <giuseppe.landolfi@gmail.com>
2019-12-10 15:01:24 +0100

    Day 10, finally

--- day_10.py
+++ day_10.py
@@ -92,3 +92,81 @@ for line in reachabilityMap:
 
 print(minimum)
 print(reachabilityMap[minimum[0]][minimum[1]])
+
+station = minimum
+
+#print(asteroids)
+#print(station)
+
+angles = []
+knownAngles = set()
+angleToAsteroids = {}
+
+
+for asteroid in asteroids:
+    delta = (asteroid[0] - station[0], asteroid[1] - station[1])
+    angle = math.atan2(delta[0], delta[1]) + math.pi/2
+
+    # print(station, asteroid, delta, angle)
+
+    if angle not in knownAngles:
+        angles.append(angle)
+        knownAngles.add(angle)
+        angleToAsteroids[angle] = []
+
+    angleToAsteroids[angle].append(asteroid)
+
+angles = sorted(angles)
+#print(angles)
+firstAngleIdx = None
+
+# find first angle:
+for angleIdx in range(len(angles)):
+    angle = angles[angleIdx]
+    if angle >= 0 and (firstAngleIdx is None or angle < angles[firstAngleIdx]):
+        firstAngleIdx = angleIdx
+
+# print(firstAngleIdx, angles[firstAngleIdx])
+
+
+for angle in angles:
+    asteroidsAtAngle = angleToAsteroids[angle]
+    angleToAsteroids[angle] = sorted(angleToAsteroids[angle], key=lambda x: (x[0] - station[0])**2 + (x[1] - station[1])**2)
+
+print(angleToAsteroids)
+
+destroyed = []
+angleIdx = firstAngleIdx
+while len(angleToAsteroids) > 0:
+    if angles[angleIdx] in angleToAsteroids:
+        asteroidsAtAngle = angleToAsteroids[angles[angleIdx]]
+
+        asteroid = asteroidsAtAngle.pop(0)
+        destroyed.append(asteroid)
+
+        print("Removing ", asteroid, " at angle ", angles[angleIdx])
+
+        if len(asteroidsAtAngle) == 0:
+            angleToAsteroids.pop(angles[angleIdx])
+
+
+    # increase the angle
+    angleIdx += 1
+    if angleIdx == len(angles):
+        angleIdx = 0
+
+print(destroyed[0])
+print(destroyed[1])
+print(destroyed[2])
+print(destroyed[9])
+print(destroyed[19])
+print(destroyed[49])
+print(destroyed[99])
+print(destroyed[198])
+print(destroyed[199])
+print(destroyed[200])
+print(destroyed[-1])
+
+bet = destroyed[199]
+
+print(bet[1]*100+bet[0])
