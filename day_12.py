136e977cbdbc92287b9c209ec1d21baa0cde9a1e
Giuseppe Landolfi <giuseppe.landolfi@gmail.com>
2019-12-14 06:20:00 +0100

    Day 12, 2

--- day_12.py
+++ day_12.py
@@ -1,9 +1,22 @@
+import math
+import functools
+
 moons = [
     [4, 1, 1, 0, 0, 0],
     [11, -18, -1, 0, 0, 0],
     [-2, -10, -4, 0, 0, 0],
     [-7, -2, 14, 0, 0, 0],
 ]
+
+
+# moons = [
+#    [ -1,  0,  2, 0, 0, 0 ],
+#    [  2,-10, -7, 0, 0, 0 ],
+#    [  4, -8,  8, 0, 0, 0 ],
+#    [  3,  5, -1, 0, 0, 0 ],
+# ]
+
+
 
 
 for moon in moons:
@@ -13,7 +26,12 @@ print()
 
 previousEnergy = 0
 
-for i in range(1000):
+i = 0
+# Find the cycle of each axis, then return the mcm
+cycles = {}
+combinations = {0:set(), 1:set(), 2:set()}
+
+while True:
 
     # gravity:
     # moon1
@@ -38,22 +56,32 @@ for i in range(1000):
         for w in [0, 1, 2]:
             moon[w] += moon[w+3]
 
-    energy = 0
-    thisRound = 0
+    for w in range(3):
+        if w in cycles:
+            continue
 
-    for moon in moons:
-        potential = 0
-        kinetic = 0
+        combination = tuple([tuple([moons[m][w] for m in range(len(moons))]),
+                             tuple([moons[m][w + 3] for m in range(len(moons))])])
 
-        for w in [0,1,2]:
-            potential += abs(moon[w])
-            kinetic += abs(moon[w+3])
+        if combination in combinations[w]:
+            cycles[w] = i
+        else:
+            combinations[w].add(combination)
 
-        thisRound += potential * kinetic
+    if len(cycles) == 3:
+        break
 
-    print(thisRound)
-    energy += thisRound
 
+    i += 1
+
+
+print(cycles)#
+
+a = list(cycles.values())
+lcm = a[0]
+for i in a[1:]:
+  lcm = lcm*i//math.gcd(lcm, i)
+print(lcm)
 
 # for moon in moons:
 #     print(moon)
